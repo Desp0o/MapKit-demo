90d106d34ce2245cd83f0116d4d9601ae23390be
desp0o <tornike.despotashvili@gmail.com>
2025-07-18 18:41:51 +0400

    very basics

---
+++ MapKit Demo/MapKit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_DemoApp.swift
+//  MapKit Demo
+//
+//  Created by Tornike Despotashvili on 7/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
